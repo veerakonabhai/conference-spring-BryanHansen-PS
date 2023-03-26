3553be572a3bdc8c27aecb41f2fd6764352e8915
VeeraKonabhai <veerareddy908@gmail.com>
2023-03-26 20:15:38 +0530

    adding speaker model

---
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,25 @@
+package com.pluralsight.model;
+
+/* Speaker model class */
+public class Speaker {
+
+//    name variables of the speaker and their getter setter methods
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
